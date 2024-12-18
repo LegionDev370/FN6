2cf191513b11f37ce832ea090ea4e7c468d4a917
LegionDev370 <llegion630@gmail.com>
2024-12-18 09:57:10 +0500

    code qo'shildi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log("hello github");
 console.log("world");
 for (let index = 0; index < 5; index++) {}
-const array = [1,2,3,4,5,6]
-const arr = []
+const array = [1, 2, 3, 4, 5, 6];
+const arr = [];
+console.log("local code");
